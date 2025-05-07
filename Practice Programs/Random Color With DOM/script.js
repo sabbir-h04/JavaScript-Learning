7fa22e8135fc65e8d65357598c92269f682aa750
sabbir-h04 <78879656+sabbir-h04@users.noreply.github.com>
2025-05-07 15:59:19 +0600

    modify randomColor function

--- Practice Programs/Random Color With DOM/script.js
+++ Practice Programs/Random Color With DOM/script.js
@@ -2,15 +2,10 @@ let boxes = document.querySelectorAll(".box");
 console.log(boxes);
 
 function randomColor() {
-  let colors = {
-    1: "red",
-    2: "blue",
-    3: "green",
-    4: "yellow",
-  };
-
-  let colorName = colors[Math.floor(Math.random() * 4) + 1];
-  return colorName;
+  let value1 = Math.floor(Math.random() * 256);
+  let value2 = Math.floor(Math.random() * 256);
+  let value3 = Math.floor(Math.random() * 256);
+  return `rgb(${value1}, ${value2}, ${value3})`;
 }
 
 boxes.forEach((e) => {
